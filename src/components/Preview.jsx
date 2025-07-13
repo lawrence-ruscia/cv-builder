24a6198e9fd5e2fc17c58a546d0a79d7f7a47cd1
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-07-14 00:00:45 +0800

    style(preview): add minor styling to personal details preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,15 +1,17 @@
 import styles from '../assets/styles/Preview.module.css';
 
 import { PersonalPreview } from './PersonalPreview';
-
+import { useRef, useState, useEffect } from 'react';
 export const Preview = ({
   personalDetails,
   educationDetails,
   professionalDetails,
 }) => {
   return (
-    <section id='preview' className={styles.preview}>
-      <PersonalPreview personalDetails={personalDetails} />
+    <section id='preview' className={styles.previewWrapper}>
+      <div className={styles.cvPreview}>
+        <PersonalPreview personalDetails={personalDetails} />
+      </div>
     </section>
   );
 };
