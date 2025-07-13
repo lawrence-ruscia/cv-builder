def3192bfec7bd89ba4aa3dd137e7cadbae69f79
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-07-13 21:54:28 +0800

    chore: add personal details mock data for easier debugging

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,16 @@ import { Details } from './Details';
 import { Preview } from './Preview';
 import { useState } from 'react';
 
+const personalMockData = {
+  fullName: 'John Doe',
+  jobTitle: 'Software Engineer',
+  email: 'johndoe@gmail.com',
+  phone: '0917 123 4567',
+  location: 'Metro Manila, Philippines',
+};
 function App() {
   const [isPreview, setIsPreview] = useState(false);
-  const [personalDetails, setPersonalDetails] = useState({});
+  const [personalDetails, setPersonalDetails] = useState(personalMockData);
 
   const handleTogglePreview = () => {
     setIsPreview((prev) => !prev);
